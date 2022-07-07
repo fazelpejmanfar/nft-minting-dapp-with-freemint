dde378854e0e8bd1f8c0760289f0f703ef09eaab
Fazelpejmanfar <fazelpejmanfar@gmail.com>
2022-07-07 05:37:05 -0700

    commit

---
+++ src/redux/data/dataActions.js
@@ -0,0 +1,42 @@
+// log
+import store from "../store";
+
+const fetchDataRequest = () => {
+  return {
+    type: "CHECK_DATA_REQUEST",
+  };
+};
+
+const fetchDataSuccess = (payload) => {
+  return {
+    type: "CHECK_DATA_SUCCESS",
+    payload: payload,
+  };
+};
+
+const fetchDataFailed = (payload) => {
+  return {
+    type: "CHECK_DATA_FAILED",
+    payload: payload,
+  };
+};
+
+export const fetchData = () => {
+  return async (dispatch) => {
+    dispatch(fetchDataRequest());
+    try {
+      let totalSupply = await store
+        .getState()
+        .blockchain.smartContract.methods.totalSupply()
+        .call();
+      dispatch(
+        fetchDataSuccess({
+          totalSupply,
+        })
+      );
+    } catch (err) {
+      console.log(err);
+      dispatch(fetchDataFailed("Could not load data from contract."));
+    }
+  };
+};
